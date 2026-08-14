a60336156491ed14b9c5627aa44976c7c7e81ae1
SamyogGhimire <samyog.ghimire25@gmail.com>
2026-08-14 16:42:27 +0530

    Remove deprecated Terraform configurations and lock files for public S3, open security group, admin IAM policy, cross-resource IAM, and secure baseline cases.

--- cases/03-admin-iam-policy/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_iam_policy" "admin_policy" {
-  name = "research-admin-policy"
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{ Effect = "Allow", Action = "*", Resource = "*" }]
-  })
-}
